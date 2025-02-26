26a6803e670d85cb4ffe8bda450cb232d48b50a6
Alexander6Shevtsov <axrsvr@gmail.com>
2025-02-26 16:44:41 +0300

    Without Repetitions

---
+++ AppForFiveMin/ViewControllers/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  AppForFiveMin
+//
+//  Created by Alexander Shevtsov on 26.02.2025.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    
+    @IBOutlet private weak var doItLabel: UILabel!
+    
+    private let doItList = [
+        "Поесть",
+        "Поспать",
+        "Прогуляться",
+        "Поесть и поспать",
+        "Погулять с собакой",
+        "Завести собаку",
+        "Завести попугая",
+        "Покормить попугая"
+    ]
+    
+    @IBAction func doItButtonPressed(_ sender: Any) {
+        doItLabel.text = doItList.randomElement() ?? "Отдыхать"
+    }
+}
